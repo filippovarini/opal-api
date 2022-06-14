7b5ef61f6f0f1718b2da4cb03cbc816d133d6907
fv220 <fv220@ic.ac.uk>
2022-06-14 12:36:21 +0100

    [fv220] chore: setup controllers

---
+++ controllers/connect.py
@@ -0,0 +1,18 @@
+# Controller to connect to external databases and perform indexing.
+# Save indexing by storing the document id ; search query ; metadata genereted
+# on our database
+
+class Connect:
+  # Store the details to connect with the external database.
+  def __init__(self, connection_details) -> None:
+    self.connection_details = connection_details
+    
+  def get_document(self, id: str, query: str):
+    print(f'getting document with id ${id} for database with query ${query}')
+
+  def update_document(self, id: str):
+    print(f'updating document ${id}')
+
+  # Save document to our Database by storing the document id and metadata.
+  def save_document(self, id: str):
+    print(f'saving document ${id}')
---
+++ controllers/search.py
@@ -0,0 +1,10 @@
+# Controller to perform the Client's search
+
+from typing import List
+
+
+class Search:
+  def get_documents(self, tags: List[str]):
+    print(f'getting documents for tags ${tags}')
+
+

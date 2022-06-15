4bce6473a9fcdcafb3c852b3dddb7a41137d28dd
fv220 <fv220@ic.ac.uk>
2022-06-15 12:38:59 +0100

    feat: add documents and tags fake_data

--- app/controllers/documents.py
+++ app/controllers/documents.py
@@ -1,13 +1,18 @@
 from typing import Dict, List, Optional
 from pydantic import BaseModel
 from enum import Enum
+import json
+
+with open("fake_data.json") as jsonFile:
+    jsonObject = json.load(jsonFile)
+    jsonFile.close()
 
 class Fields(BaseModel):
   title: Optional[str]
   language: Optional[str]
 
 class UserSearch(BaseModel):
-  tags: Optional[List[str]] = []
+  tags: Optional[List[str]] = []       # list of tag ids
   keywords: Optional[List[str]] = []
   fields: Optional[Fields] = {}
 
@@ -26,16 +31,23 @@ class Document(BaseModel):
   format: DocumentFormat
   source_db: SourceDB
 
+
+documents = jsonObject['documents']
+
 # Controller to perform the Client's documentsearch
 class Document:
 
   # Searches the document from metadata stored in our DB (tags, fiels)
   def get_from_metadata(self, tags: List[str], fields: Fields) -> List[Document]:
-    return f'getting documents for tags ${tags} and fields ${fields}'
+    # Checks whether BIG list has all elements of SMALL
+    def isSublist(small: list, big: list) -> bool:
+      return all(elem in big for elem in small)
+  
+    return [doc for doc in documents if isSublist(tags, doc['tags'])]
 
   # Searches the document based on keywrods. Need to access the document text
   def get_from_keywords(self, keywords: List[str]) -> List[Document]:
-    return f'getting documents for keywords: ${keywords}'
+    return []
 
   
 documentController = Document()

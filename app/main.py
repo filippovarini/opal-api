143bdf0b967236d9cb15959a4dbe08e844daf62d
fv220 <fv220@ic.ac.uk>
2022-06-15 16:54:22 +0100

    fix: add allow CORS middleware

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from routes.router import api_router
 from core.config import settings
 
 app = FastAPI()
+
+ORIGINS = ['http://localhost:3000']
+
+app.add_middleware(
+  CORSMiddleware, 
+  allow_origins=ORIGINS,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"]
+  )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)

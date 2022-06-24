7a9a609789872e674346b2654c56c127b2fc6ee6
Robert Stok <rs.stok@gmail.com>
2022-06-24 14:49:56 +0100

    [rs1720] feature: add endpoints to allow users to create and retrieve tags that save their searches

--- app/controllers/tagController.py
+++ app/controllers/tagController.py
@@ -1,5 +1,7 @@
+from typing import List, Dict
 from unittest import result
 from pydantic import BaseModel
+from db.db_controller import database
 import json
 
 # Define the Tag class
@@ -22,10 +24,15 @@ class TagController:
   def get_from_substring(self, substr: str, username: str = None): 
     print(username)
     tag_retrieved = [tag for tag in tags if substr.lower() in tag.name.lower()]
+    user_tags = database.suggest_user_search_tags(substr)
+    tag_retrieved = tag_retrieved + user_tags
     return sorted(tag_retrieved, key=lambda tag: tag.name)
 
   def get_from_ids(self, ids: str): 
     return [tag for tag in tags if tag.id in ids]
+
+  async def add_search_tag(self, username: str, tag_name: str, result_ids: List[str], search):
+    await database.add_user_search_tag(username, tag_name, result_ids, search)
 
 
 controller = TagController()

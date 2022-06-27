a96a4c2856335bce430e9fbfdee7a83f493e1a3c
Robert Stok <rs.stok@gmail.com>
2022-06-27 16:52:22 +0100

    [rs1720] fix: auth is now awaited in the /user/get_tags endpoint, so the user's tags are also retrieved

--- app/routes/endpoints/tags.py
+++ app/routes/endpoints/tags.py
@@ -6,12 +6,12 @@ from controllers.userController import userController
 router = APIRouter()
 
 @router.get("/{tag_substring}")
-def tags_from_substring(tag_substring: str, request: Request, response: Response):
+async def tags_from_substring(tag_substring: str, request: Request, response: Response):
   username = request.headers.get('username')
   password = request.headers.get('password')
   if username is not None and password is not None:
     # check if username and password is correct
-    if not userController.auth_user(username, password):
+    if not await userController.auth_user(username, password):
       response.status_code = status.HTTP_401_UNAUTHORIZED
       username = None
 
